0182b421ef37eecd3b037baa6a10a9fad54aac29
Yina Zhu <yinagit@gmail.com>
2018-02-04 20:43:09 +0000

    fixed user input between 1-12 issue

--- lib/twittascope/cli.rb
+++ lib/twittascope/cli.rb
@@ -1,4 +1,5 @@
 #Our CLI Controller
+
 class Twittascope::CLI
 
   def call
@@ -14,47 +15,48 @@ class Twittascope::CLI
     puts "Type in the number of your Horoscope sign!"
   end
 
+  def option
+    puts "Type 'A' to learn about your fortune tomorrow."
+    puts "Or type 'B' to main menu."
+  #   input = nil
+  #   while input != "exit"
+  #   input = gets.strip.downcase
+  #   case input
+  #     when "a"
+  #       tomorrow_horoscope
+  #     when "b"
+  #       list_horoscope
+  #     else
+  #       "Oops! We don't recognize! Please try again!"
+    end
+  # end
+
   def today_horoscope
     puts "You will be very lucky today."
     option
   end
+
 
   def tomorrow_horoscope
     puts "You are reading tomorrow's horoscope"
     option
   end
 
-  def option
-    puts "Type 'A' to learn about your fortune tomorrow."
-    puts "Or type 'B' to main menu."
-    input = nil
-    while input != "exit"
-    input = gets.strip.downcase
-    case input
-      when "a"
-        tomorrow_horoscope
-      when "b"
-        list_horoscope
-      else
-        "Oops! We don't recognize! Please try again!"
-    end
-  end
-
   def selection
     input = nil
     while input != "exit"
       input = gets.strip.downcase
-      #horoscope will load
-      case input
-      when "1" #the user inputs between 1 to 12
+      # case input
+      if input.to_i.between?(1,12)
         today_horoscope
-        option
       else
         puts "Oops! Please try again!"
         list_horoscope
       end
     end
   end
+
+
 
   def goodbye
     puts "Goodluck!"
