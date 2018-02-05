4ecb3b6cbe469537699a2dc97d8f81741ce96233
Yina Zhu <yinagit@gmail.com>
2018-02-05 02:06:48 +0000

    CLI added iteration over name and horoscope

--- lib/twittascope/cli.rb
+++ lib/twittascope/cli.rb
@@ -10,35 +10,44 @@ class Twittascope::CLI
   end
 
   def list_horoscope
-    puts "1. Aries"
-    puts "2. Taurus"
-    puts "Type in the number of your Horoscope sign!"
+    @horoscope = Twittascope::Horoscope.today
+    @horoscope.each.with_index(1) do |sign, index|
+      puts "#{index}. #{sign.name}"
   end
+end
+
+  def selection
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+
+    if input.to_i.between?(1,12)
+      #will scrape twittascope's today horoscope from website
+      @horoscope = Twittascope::Horoscope.today
+      puts @horoscope[input.to_i-1].new
+      option
+    else
+      puts "Oops! Please try again!"
+    end
+  end
+end
 
   def option
     puts "Type 'A' to learn about your fortune tomorrow."
     puts "Or type 'B' to main menu."
-  #   input = nil
-  #   while input != "exit"
-  #   input = gets.strip.downcase
-  #   case input
-  #     when "a"
-  #       tomorrow_horoscope
-  #     when "b"
-  #       list_horoscope
-  #     else
-  #       "Oops! We don't recognize! Please try again!"
+    input = nil
+    while input != "exit"
+    input = gets.strip.downcase
+    case input
+      when "a"
+        tomorrow_horoscope
+      when "b"
+        list_horoscope
+      else
+        "Oops! We don't recognize! Please try again!"
     end
-  # end
-
-  def today_horoscope
-    #scrapes today's horoscope
-    #date
-    #horoscope
-    # puts "You will be very lucky today."
-    @horoscope = Twittascope::Horoscope.today
-    option
   end
+end
 
 
   # def tomorrow_horoscope
@@ -48,19 +57,7 @@ class Twittascope::CLI
   #   option
   # end
 
-  def selection
-    input = nil
-    while input != "exit"
-      input = gets.strip.downcase
-      if input.to_i.between?(1,12)
-        today_horoscope
-        option
-      else
-        puts "Oops! Please try again!"
-        list_horoscope
-      end
-    end
-  end
+
 
 
   def goodbye
--- lib/twittascope/horoscope.rb
+++ lib/twittascope/horoscope.rb
@@ -1,19 +1,23 @@
 class Twittascope::Horoscope
 
+attr_accessor :name, :new
 
 def self.today
   #should return user input's today's horoscope
-  puts "You are reading today's horoscope."
+  # puts "You are reading today's horoscope."
+ horoscope_1 = self.new
+ horoscope_1.name = "Aries"
+ horoscope_1.new = "Aries. pull from website"
 
-  horoscope_1 = self.new
-  horoscope_1.name = "Aries"
-  horoscope_1.today = "You are reading today's horoscope."
+ horoscope_2 = self.new
+ horoscope_2.name = "Taurus"
+ horoscope_2.new = "Taurus. pull from website"
 
-  horoscope_2 = self.new
-  horoscope_2.name = "Taurus"
-  horoscope_2.today = "You are reading today's horoscope."
+ horoscope_3 = self.new
+ horoscope_3.name = "Gemini"
+ horoscope_3.new = "Gemini. pull from website"
 
-
-
+ [horoscope_1, horoscope_2, horoscope_3]
+end
 
 end
