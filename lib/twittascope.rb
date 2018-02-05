69dd8857514ea371b0a527de39d57f6e8620601f
Yina Zhu <yinagit@gmail.com>
2018-02-05 02:30:53 +0000

    added nokogiri/pry

--- lib/twittascope.rb
+++ lib/twittascope.rb
@@ -3,3 +3,7 @@ require_relative "./twittascope/version"
 require_relative './twittascope/cli'
 
 require_relative './twittascope/horoscope'
+
+# require "pry"
+# require "nokogiri"
+# require "rspec"
--- lib/twittascope/horoscope.rb
+++ lib/twittascope/horoscope.rb
@@ -4,20 +4,26 @@ attr_accessor :name, :new
 
 def self.today
   #should return user input's today's horoscope
-  # puts "You are reading today's horoscope."
- horoscope_1 = self.new
- horoscope_1.name = "Aries"
- horoscope_1.new = "Aries. pull from website"
+  self.scrape_horoscope
+end
 
- horoscope_2 = self.new
- horoscope_2.name = "Taurus"
- horoscope_2.new = "Taurus. pull from website"
+def self.scrape_horoscope
+  Twittascope::HoroscopeScraper.new("http://www.twittascope.com")
+  horoscopes = []
 
- horoscope_3 = self.new
- horoscope_3.name = "Gemini"
- horoscope_3.new = "Gemini. pull from website"
+  horoscope_1 = self.new
+  horoscope_1.name = "Aries"
+  horoscope_1.new = "Aries. pull from website"
 
- [horoscope_1, horoscope_2, horoscope_3]
+  horoscope_2 = self.new
+  horoscope_2.name = "Taurus"
+  horoscope_2.new = "Taurus. pull from website"
+
+  horoscope_3 = self.new
+  horoscope_3.name = "Gemini"
+  horoscope_3.new = "Gemini. pull from website"
+
+  horoscopes
 end
 
 end
