e413815435cfe3d0e5abf5dfebf39f23f5fa8948
Yina Zhu <yinagit@gmail.com>
2018-03-01 05:31:27 +0000

    housekeeping

--- lib/twittascope/cli.rb
+++ lib/twittascope/cli.rb
@@ -1,14 +1,11 @@
 require 'colorize'
 
-# CLI BELOW
-================================================================================
 class Twittascope::CLI
-  
+
   def self.play
     welcome_menu
-    display_horoscope
+    # display_horoscope
   end
-
 
   def self.welcome_menu
     # displays horoscope names and birth dates
@@ -19,52 +16,54 @@ class Twittascope::CLI
       #condition input to go to an index of that list
       #you will have an instance
 
-      Horoscope.find_by(name)
-
-      Scraper.load_page
-  end
-
-  def display_horoscope
-      input = nil
-      while input != "exit"
-        input = gets.to_i
-
-      if input.between?(1,13)
-        @horoscope = Twittascope::Horoscope.forecast_today
-        puts @horoscope[input]
-        more_options
-      elsif input == "13"
-        @horoscope = Twittascope::Horoscope.dontknow
-        #(****put this on its own scraper method? or its own variable***)
-      else
-        puts "Oops! Please try again!"
-      end
-    end
+      # Horoscope.find_by(name)
+      #
+      # Scraper.load_page
   end
 
 
-  def more_options
-    puts "Type 'A' to learn about another horoscope sign."
-    puts "Or type 'B' to exit."
-    input = nil
-    while input != "exit"
-    input = gets.strip.downcase
-    case input
-      when "a"
-        welcome_menu
-      when "b"
-        goodbye
-      else
-        "Oops! We don't recognize! Please try again!"
-        welcome_menu
-    end
-  end
-end
+
+  # def display_horoscope
+  #     input = nil
+  #     while input != "exit"
+  #       input = gets.to_i
+  #
+  #     if input.between?(1,13)
+  #       @horoscope = Twittascope::Horoscope.forecast_today
+  #       puts @horoscope[input]
+  #       more_options
+  #     elsif input == "13"
+  #       @horoscope = Twittascope::Horoscope.dontknow
+  #       #(****put this on its own scraper method? or its own variable***)
+  #     else
+  #       puts "Oops! Please try again!"
+  #     end
+  #   end
+  # end
+
+
+#   def more_options
+#     puts "Type 'A' to learn about another horoscope sign."
+#     puts "Or type 'B' to exit."
+#     input = nil
+#     while input != "exit"
+#     input = gets.strip.downcase
+#     case input
+#       when "a"
+#         welcome_menu
+#       when "b"
+#         goodbye
+#       else
+#         "Oops! We don't recognize! Please try again!"
+#         welcome_menu
+#     end
+#   end
+# end
 
 end  #closes Twittascope::CLI
 
 
-============================================================================================================
+
 
 
 #
--- lib/twittascope/scraper.rb
+++ lib/twittascope/scraper.rb
@@ -1,18 +1,30 @@
+require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
-require 'pry'
 class Scraper
-#============================================================
+
   def self.load_horoscopes
+    horoscopes = []
     doc = Nokogiri::HTML(open("http://www.twittascope.com"))
 
-    main = doc.search("ul.site-sign-list li a").each do |t|
+      main = doc.search("ul.site-sign-list li a").each do |t|
       url = "http://www.twittascope.com" + t["href"]
-      name = url.gsub("http://www.twittascope.com/?sign=", "").capitalize
 
-      Horoscope.new(url, name)
+      name = url.gsub("http://www.twittascope.com/?sign=", "").capitalize
+      # horoscopes << {name: sign_name, url: sign_url}
+      horoscopes << name
+      # binding.pry
+      horoscopes.each_with_index do |sign, index|
+      index += 1
+      puts "#{index}. #{sign}"
+      horoscopes
+
+      # Horoscope.new(url, name)
     end
+
   end
+end
 
   def self.load_page(instance)
 
@@ -32,40 +44,6 @@ class Scraper
     # container = html.css(".dh-copy p").first
     #
     # today = container.text.strip
-# def self.horoscope_list
-#   page = Nokogiri::HTML(open("http://www.twittascope.com"))
-#   # signs = []
-#   page.css("li.sign-item").each_with_index do |sign, index|
-#     puts("#{index+1}. #{sign.text.strip}")
-#   # signs
-#   end
-# end
-#
-# def self.forecast_today
-#     sub_page = Nokogiri::HTML(open("http://www.twittascope.com"))
-#     today = sub_page.css(".dh-copy p").map do |today|
-#       puts "#{today.text.strip}"
-#
-#     end
-#   end
-#
-# end
-#
-#
-# # def self.forecast_tomorrow
-# #   page = Nokogiri::HTML(open("http://www.twittascope.com")) #this should scrape subpages
-# #   tomorrow_news = []
-# #   page.css("sign=").each do |name|
-# #   horoscope = Horoscope.new
-# #   horoscope.tomorrow = page.css("something")
-# #   tomorrow_news
-# # end
-#
-# #
-# # def self.horoscope_forecast(signs)
-# #   horoscopes = {}
-# #   sign_pages = Nokogiri::HTML(open(signs))
-# #   sign_links
-# # end
-#
+
+
 end
