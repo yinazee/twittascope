e02d33e59ccf555aa9f285b76dd9f44f5b31993a
Yina Zhu <yinagit@gmail.com>
2018-03-16 21:29:54 +0000

    why isn't welcome loop working

--- lib/twittascope/cli.rb
+++ lib/twittascope/cli.rb
@@ -3,39 +3,64 @@ require 'colorize'
 class Twittascope::CLI
 
   def self.play
+    work
     welcome
-
+    display_today
   end
+
+  def self.work
+    input = nil
+    while input != "x"
+      input = gets
+      if input == "a"
+      puts "oh wells"
+    elsif input == "b"
+      puts "okay"
+    else
+      puts "try again"
+    end
+  end
+  binding.pry
+end
+
 
   def self.welcome
     puts "Welcome to Twittascope!".yellow
     puts "Type in the number of your Horoscope sign!".yellow
       @headlines = Scraper.scrape_headline
        @headlines[0..12].each_with_index do |h1, index|
-         puts "#{index+1}. #{h1}"
+         puts "#{index+1}. #{h1}".colorize(:blue)
        end
   end
 
-  
-#
-#   def self.display_today
-#     input = nil
-#     while input != "exit"
-#     input = gets.to_i
-#   if input.between?(1,14)
-#     #will scrape twittascope's today horoscope from website
-#     @horoscope = Scraper.today
-#     puts @horoscope[input]
-#     more_options
-#   else
-#     puts "Oops! Please try again!"
-#   end
-# end
-# end
+
+  def goodbye
+    puts "Goodluck!"
+  end
+
+  def self.more_options
+    puts "more options"
+  end
+
+  def self.display_today
+      input = nil
+      while input != "x"
+      input = gets.to_i
+      if input.between?(1,13)
+      @headlines = Scraper.scrape_headline
+      puts @headlines[input-1].colorize(:red)
+      @today = Scraper.scrape_today
+      puts @today[input-1].colorize(:red)
+    else
+      puts "Oops! Please enter a number 1 to 13!"
+      puts "Or type 'x' to exit."
+    end
+  end
+  end
 
 
+  # def more_options
 
-#   def more_options
 #     puts "Type 'A' to learn about another horoscope sign."
 #     puts "Or type 'B' to exit."
 #     input = nil
@@ -81,6 +106,3 @@ end  #closes Twittascope::CLI
 # # end
 # #
 #
-#   def goodbye
-#     puts "Goodluck!"
-#   end
