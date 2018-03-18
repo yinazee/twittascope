56ab6f3f58144812a55be2e71f8df0a9044cd6b0
Yina Zhu <yinagit@gmail.com>
2018-03-18 20:50:28 +0000

    more_otpions structure set up. exit works.

--- lib/twittascope/cli.rb
+++ lib/twittascope/cli.rb
@@ -3,25 +3,11 @@ require 'colorize'
 class Twittascope::CLI
 
   def self.play
-    # work
     welcome
     display_today
+    more_options
     goodbye
   end
-
-  def self.work
-    input = nil
-    while input != "x"
-      input = gets
-      if input == "a"
-      puts "oh wells"
-    elsif input == "b"
-      puts "okay"
-    else
-      puts "try again"
-    end
-  end
-end
 
 
   def self.welcome
@@ -39,75 +25,47 @@ end
   end
 
   def self.more_options
-    puts "more options"
-  end
+     puts "Type 'list' to see the list again."
+     puts "Type 'yesterday' to see yesterday's horoscope."
+     puts "Type 'tomorrow' to see tomorrow's horoscope."
+     puts "Type 'exit' to exit."
+      input = nil
+      while input != "x"
+      input = gets.chomp.downcase
+        if input == "list"
+          welcome
+        elsif input == "yesterday"
+          puts "you are reading yesterday's horoscope."
+        elsif input == "tomorrow"
+          puts "you are reading tomorrow's horoscope."
+        elsif input == "x"
+          puts "Goodbye! Come back to check your fortune!"
+        else
+          puts "not sure what you mean, try again."
+        end
+      end
+    end
+
 
   def self.display_today
       input = nil
-      while input != "exit"
+      while input != "x"
       input = gets.chomp.downcase
       if input.to_i.between?(1,13)
       @headlines = Scraper.scrape_headline
       puts @headlines[input.to_i-1].colorize(:red)
       @today = Scraper.scrape_today
       puts @today[input.to_i-1].colorize(:red)
-    elsif input == "exit"
+      more_options
+    elsif input == "x"
       puts "Goodbye! Come back to check your fortune!"
       exit
     else
       puts "Oops! Please enter a number 1 to 13!"
-      puts "Or type 'exit' to exit."
+      puts "Or type 'x' to exit."
     end
   end
 end
 
 
-
-
-  # def more_options
-
-#     puts "Type 'A' to learn about another horoscope sign."
-#     puts "Or type 'B' to exit."
-#     input = nil
-#     while input != "exit"
-#     input = gets.strip.downcase
-#     case input
-#       when "a"
-#         welcome_menu
-#       when "b"
-#         goodbye
-#       else
-#         "Oops! We don't recognize! Please try again!"
-#         welcome_menu
-#     end
-#   end
-# end
-
 end  #closes Twittascope::CLI
-
-
-
-
-
-#
-#
-#
-#
-# #   def tomorrow_horoscope
-# #     #scrapes tomorrow's horoscope
-# #     input = nil
-# #     puts "You are reading tomorrow's horoscope"
-# #
-# #     @horoscope = Twittascope::Horoscope.tomorrow
-# #
-# #       #will scrape twittascope's today horoscope from website
-# #
-# #       puts @horoscope[input.to_i-1]
-# #       more_options
-# #     else
-# #       puts "Oops! Please try again!"
-# #     end
-# #   end
-# # end
-# #
-#
